5ec131ee420e96618e97ab15a349142d9b8f59b9
Faruq <faruqadam.92@gmail.com>
2024-03-21 17:38:09 +0100

    In progress

---
+++ python_projects/password_encryption.py
@@ -0,0 +1,31 @@
+master_pwd = input("What is your master password? ")
+
+def view():
+    with open('passwords.txt', 'r') as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, passw = data.split("|")
+            print("User:", user, "Password:", passw)
+         
+def add():
+    name = input("Account Name: ")
+    pwd = input("Password: ")
+    
+    with open('passwords.txt', 'a') as f:
+        f.write(name + "|" + pwd + "\n") 
+
+
+
+while True:
+    mode = input("would like to create or view password? (Add/View) or q to Quit: ").lower()
+
+    if mode == "q":
+        print("Bye!!!")
+        break
+    elif mode == "view":
+        view()
+    elif mode == "add":
+        add()
+    else:
+        print("Invalid selection")
+        continue
