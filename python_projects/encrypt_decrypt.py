16f76ff97bf77d05b0485d42c29d1c9fdce095e3
Faruq <faruqadam.92@gmail.com>
2024-03-23 06:58:58 +0100

    Finished project

--- python_projects/encrypt_decrypt.py
+++ python_projects/encrypt_decrypt.py
@@ -4,7 +4,32 @@ import base64
 import os
 
 def decrypt():
-    print()
+     password = code.get()
+     
+     if password == "1234":
+        screen2 = Toplevel(screen)
+        screen2.title("decryption")
+        screen2.geometry("400x200")
+        screen2.configure(bg="white")
+
+        message = text1.get(1.0,END)
+        decode_message = message.encode("ascii")
+        base64_bytes = base64.b64decode(decode_message)
+        decrypt = base64_bytes.decode("ascii")
+
+        Label(screen2, text="DECRYPT", font="arial", fg="white", bg="#00bd56").place(x=10, y=0)
+        text2 = Text(screen2, font="Robote 10", bg="white", relief=GROOVE, wrap=WORD,bd=0)
+        text2.place(x=10, y=40, width=380, height=150)
+
+        text2.insert(END,decrypt)
+
+                
+     elif password == "":
+        messagebox.showerror("encryption", "Input Password")
+
+     elif password != "":
+        messagebox.showerror("encryption", "Invalid Password")
+
 
 
 def encrypt():
@@ -14,11 +39,11 @@ def encrypt():
         screen1 = Toplevel(screen)
         screen1.title("encryption")
         screen1.geometry("400x200")
-        screen1.configure(bg="#ed3833")
+        screen1.configure(bg="white")
 
         message = text1.get(1.0,END)
         encode_message = message.encode("ascii")
-        base64_bytes = base64.b64decode(encode_message)
+        base64_bytes = base64.b64encode(encode_message)
         encrypt = base64_bytes.decode("ascii")
 
         Label(screen1, text="ENCRYPT", font="arial", fg="white", bg="#ed3833").place(x=10, y=0)
@@ -27,11 +52,11 @@ def encrypt():
 
         text2.insert(END,encrypt)
 
+    elif password == "":
+         messagebox.showerror("encryption", "Input Password")
 
-
-
-
-
+    elif password != "":
+         messagebox.showerror("encryption", "Invalid Password")
 
 
 def main_screen():
