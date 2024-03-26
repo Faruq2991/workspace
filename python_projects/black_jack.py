9288c081d03ec04f1ea00860afab571a6ad520c0
Faruq <faruqadam.92@gmail.com>
2024-03-26 08:18:36 +0100

    Black Jack: A card game.

--- python_projects/black_jack.py
+++ python_projects/black_jack.py
@@ -1,58 +1,40 @@
 import random
-cards = []
-suits = ["hearts", "spades", "clubs", "diamonds"]
-ranks = [
-        {"rank":"A", "value": "11"}, 
-         {"rank":"2", "value": "2"}, 
-         {"rank":"3", "value": "3"}, 
-         {"rank":"4", "value": "4"},
-         {"rank":"5", "value": "5"}, 
-         {"rank":"6", "value": "6"}, 
-         {"rank":"7", "value": "7"}, 
-         {"rank":"8", "value": "8"},
-         {"rank":"9", "value": "9"},
-         {"rank":"10","value": "10"}, 
-         {"rank":"J", "value": "10"},
-         {"rank":"Q", "value": "10"}, 
-         {"rank":"K", "value": "10"},
-]
-    
-for suit in suits:
-    for rank in ranks:
-        cards.append([suit,rank])
+class Deck:
+    def __init__(self) -> None:
+        pass
+        self.cards = []
+        suits = ["hearts", "spades", "clubs", "diamonds"]
+        ranks = [
+                {"rank":"A", "value": "11"}, 
+                {"rank":"2", "value": "2"}, 
+                {"rank":"3", "value": "3"}, 
+                {"rank":"4", "value": "4"},
+                {"rank":"5", "value": "5"}, 
+                {"rank":"6", "value": "6"}, 
+                {"rank":"7", "value": "7"}, 
+                {"rank":"8", "value": "8"},
+                {"rank":"9", "value": "9"},
+                {"rank":"10","value": "10"}, 
+                {"rank":"J", "value": "10"},
+                {"rank":"Q", "value": "10"}, 
+                {"rank":"K", "value": "10"},
+        ]
+            
+        for suit in suits:
+            for rank in ranks:
+                self.cards.append([suit,rank])
 
-def shuffle():
-    random.shuffle(cards)
+    def shuffle(self):
+        random.shuffle(self.cards)
 
-def deal(number):
-    cards_dealt = []
-    for card in range(number):
-        card = cards.pop()
-        cards_dealt.append(card)
-    return cards_dealt
-
-shuffle()
-
-card = deal(3)[0]
-print(card[1]["value"])
-
-
-
-
-
-
-"""cards_dealt = deal(2)
-card = cards_dealt[0]
-rank = card[1]
-
-if rank == "A":
-    value = 11
-elif rank == "J" or rank == "Q" or rank == "K":
-    value = 10
-else: value = rank
-
-
-rank_dict = {"rank": rank, "value": value}
-
-print(rank_dict["rank"], rank_dict["value"])
-"""
+    def deal(self, number):
+        cards_dealt = []
+        for card in range(number):
+            card = self.cards.pop()
+            cards_dealt.append(card)
+        return cards_dealt
+deck_1 = Deck()
+deck_2 = Deck()
+deck_2.shuffle()
+print(deck_1.cards)
+print(deck_2.cards)
